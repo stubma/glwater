f48df760b3a76127c0f2c30d2705f80dc1da5ac9
luma <stubma@gmail.com>
2015-01-11 18:32:57 +0800

    x

--- ios/glwater/Texture2D.mm
+++ ios/glwater/Texture2D.mm
@@ -51,8 +51,8 @@
 - (id)initWithSize:(CGSize)size withType:(GLenum)type {
     if(self = [super init]) {
         self.target = GL_TEXTURE_2D;
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (int)size.width, (int)size.height, 0, GL_RGBA, type, nullptr);
